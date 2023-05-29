7a7d9c9fefa93284f9460be4781ca8b620cd6160
Tarmica <68661064+lordskyzw@users.noreply.github.com>
2023-05-30 00:34:41 +0200

    better prompt engineering

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ import os
 from tools import *
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
-
-from langchain.chat_models import ChatOpenAI
+# from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
 from langchain import PromptTemplate
 from langchain.vectorstores import Pinecone
@@ -12,8 +11,8 @@ import pinecone
 from langchain.llms import OpenAI
 from pymongo import MongoClient
 
-llm = ChatOpenAI(
-    model_name="gpt-3.3-turbo",
+llm = OpenAI(
+    model_name="text-davinci-003",
     temperature=0.7,
     openai_api_key=os.environ.get("OPENAI_API_KEY"),
 )
@@ -48,9 +47,9 @@ def chat():
     recipient_obj = {"id": recipient, "phone_number": recipient}
     collection.insert_one(recipient_obj)
 
-    # Get the message, make vector embeddings
+    # Get the message
     message = request.form.get("Body")
-    vectorstore = Pinecone(index, embeddings.embed_query, "text")
+    
 
     # Load up the specific user chat history from  the mongodb
     history = dbconnection(recipient)
@@ -60,13 +59,15 @@ def chat():
 
     # begin Prompt Engineering
     template = """    
-    {semantic_memories}
     
-    {chat_history}
     
-    You're an AI named Winter.
-    You were created by a computer engineer (23 year old Zimbabwean male) named Tarmica Chiwara in 2023 who is currently enrolled at Chinhoyi University of Technology. 
-    You're having a conversation with another human. Use the above as semantic memories & recent chat history, if there is none, proceed naturally.
+    You're a "whatsapp accessed" AI named Winter.
+    You were created by a 23 year old Zimbabwean male computer engineer named Tarmica Chiwara in 2023 who is currently enrolled at Chinhoyi University of Technology. 
+    You're having a conversation with a human. Use information below to respond naturally. If you don't know the answer, please admit
+
+    <begining of semantic memories sparked by recent user input>{semantic_memories}<begining of semantic memories sparked by recent user input>
+    
+    <beginning of recent 5 texts>{chat_history}<end of resent 5 texts>
 
     Respond as the AI to the following interaction:
     Human: {human_input}
@@ -80,12 +81,18 @@ def chat():
         prompt=prompt,
         verbose=True,
     )
-
+    vectorstore = Pinecone(index, embeddings.embed_query, "text")
     chat_history = str(history.messages[-5:]).replace(
         ", additional_kwargs={}, example=False", ""
     )
     chat_history.replace("content=", "")
 
+
+
+
+
+
+    # get response from the llm
     dic = {
         "semantic_memories": str(
             vectorstore.similarity_search(query=message, k=1, namespace=recipient)
@@ -93,8 +100,6 @@ def chat():
         "chat_history": chat_history,
         "human_input": message,
     }
-
-    # get response from the llm
     reply = llm_chain.run(dic)
 
     # save the interaction to Mongo and Vectorstore
