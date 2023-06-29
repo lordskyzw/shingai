573ad3b3563765187207a3172e0e9f48cb1659e3
Tarmica <68661064+lordskyzw@users.noreply.github.com>
2023-06-29 12:43:55 +0200

    live_tweaks

--- jobs/tools.py
+++ jobs/tools.py
@@ -15,16 +15,17 @@ def get_recipient_chat_history(recipient):
         history = MongoDBChatMessageHistory(
             connection_string="mongodb://mongo:Szz99GcnyfiKRTms8GbR@containers-us-west-4.railway.app:7055",
             database_name="test",
-            session_id=recipient,
+            session_id=str(recipient),
         )
         return history
 
     except Exception as e:
         return str(e)
-    
+
+
 def clean_history(history):
-    '''does string operations to clean the history therefore reducing the size of the prompt sent to the llm'''
-    clean_history = str(history.messages[-5:]).replace( # type: ignore
+    """does string operations to clean the history therefore reducing the size of the prompt sent to the llm"""
+    clean_history = str(history.messages[-5:]).replace(
         ", additional_kwargs={}, example=False", ""
     )
     clean_history.replace("content=", "")
@@ -35,7 +36,7 @@ def clean_history(history):
 
 ######################################## users database functions ########################################
 def recipients_database():
-    '''users' database connection object'''
+    """users' database connection object"""
     client = MongoClient(
         "mongodb://mongo:Szz99GcnyfiKRTms8GbR@containers-us-west-4.railway.app:7055"
     )
@@ -43,7 +44,9 @@ def recipients_database():
     collection = database["recipients"]
     return collection
 
+
 #########################################  semantic memories functions  ########################################
+
 
 def create_or_update_semantic_memories(recipient):
     """the function to run at the end of the day"""
@@ -58,18 +61,18 @@ def create_or_update_semantic_memories(recipient):
     )
     history_string = str(history.messages)
     # create vector embeddings object
-    embeddings = OpenAIEmbeddings() # type: ignore
+    embeddings = OpenAIEmbeddings()  # type: ignore
     # create vectorstore object
     vectorstore = Pinecone(
         embeddings.embed_query, "text", index="thematrix", namespace=recipient
-    ) # type: ignore
+    )  # type: ignore
     vector_embeddings = [
         [0.2, 0.4, -0.1, 0.8, -0.5],
         [-0.3, 0.6, 0.9, -0.2, 0.1],
         [0.7, -0.5, 0.3, 0.1, -0.9],
     ]
 
-    index.upsert(vectors=vector_embeddings, namespace=recipient) # type: ignore
+    index.upsert(vectors=vector_embeddings, namespace=recipient)  # type: ignore
     sleep(5)
     try:
         vectorstore.add_texts(texts=history_string, namespace=recipient)
@@ -79,13 +82,14 @@ def create_or_update_semantic_memories(recipient):
             "An error occurred while adding texts to the vector store."
         ) from e
 
+
 def get_semantic_memories(message, recipient):
     # create vector embeddings object
-    embeddings = OpenAIEmbeddings()# type: ignore
+    embeddings = OpenAIEmbeddings()  # type: ignore
     # create vectorstore object
     vectorstore = Pinecone(
         embeddings.embed_query, "text", index="thematrix", namespace=recipient
-    )# type: ignore
+    )  # type: ignore
     # get semantic results
     try:
         semantic_results = vectorstore.similarity_search(
@@ -95,12 +99,13 @@ def get_semantic_memories(message, recipient):
     except Exception:
         return None
 
+
 def summarize_memories(semantic_memories):
     llm = OpenAI(
         openai_api_key=os.environ.get("OPENAI_API_KEY"),
         model="gpt-3.5-turbo",
         temperature=0,
-    )# type: ignore
+    )  # type: ignore
     prompt = PromptTemplate(
         input_variables=[semantic_memories],
         template="""summarize the following semantic memory documents to a degree enough for an LLM to understand:
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from utilities.promptengineering import *
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
 from twilio.rest import Client
-from langchain.chat_models import ChatOpenAI
+from langchain.llms import OpenAI
 from langchain.chains import LLMChain
 from langchain import PromptTemplate
 from langchain.vectorstores import Pinecone
@@ -12,19 +12,18 @@ from langchain.embeddings import OpenAIEmbeddings
 import pinecone
 
 
+account_sid = os.environ.get("TWILIO_SID")
+auth_token = os.environ.get("TWILIO_AUTH_TOKEN")
+twilio_client = Client(account_sid, auth_token)
 # setting up the llm, pineone object and embeddings model
-llm = ChatOpenAI(
-    model_name="gpt-3.5-turbo-0613", 
-    temperature=0.9,
-    openai_api_key=os.environ.get("OPENAI_API_KEY"),
-)
+llm = OpenAI(model="gpt-3.5-turbo")
 openai_api_key = os.environ.get("OPENAI_API_KEY")
 pinecone.init(
-    api_key=os.environ.get("PINECONE_API_KEY"), 
+    api_key=os.environ.get("PINECONE_API_KEY"),
     environment="northamerica-northeast1-gcp",
 )
 index = pinecone.Index(index_name="thematrix")
-embeddings = OpenAIEmbeddings() 
+embeddings = OpenAIEmbeddings()
 
 # users' database connection object and vectorstore:
 
@@ -59,12 +58,11 @@ app = Flask(__name__)
 
 @app.route("/chat", methods=["POST"])
 def chat():
-
-##########################################  phone number operations  ############################################
+    ##########################################  phone number operations  ############################################
 
     recipient = request.form.get("From")
     # Strip special characters and formatting from the phone number
-    recipient = "".join(filter(str.isdigit, recipient)) 
+    recipient = "".join(filter(str.isdigit, recipient))
     recipient_obj = {"id": recipient, "phone_number": recipient}
 
     # Save the recipient's phone number in the mongo user if not registred already database
@@ -72,17 +70,16 @@ def chat():
         recipients_db.insert_one(recipient_obj)
 
     history = get_recipient_chat_history(recipient)
-    #cleaning the history
+    # cleaning the history
     chat_history = clean_history(history)
 
-
-##########################################  message operations  ############################################
+    ##########################################  message operations  ############################################
 
     message = request.form.get("Body")
     # get response from the llm
     dic = {
         "semantic_memories": str(
-            vectorstore.similarity_search(query=message, k=1, namespace=recipient) 
+            vectorstore.similarity_search(query=message, k=3, namespace=recipient)
         ).replace(", metadata={}", ""),
         "chat_history": chat_history,
         "human_input": message,
@@ -90,14 +87,18 @@ def chat():
     reply = llm_chain.run(dic)
 
     # save the interaction to Mongo
-    history.add_user_message(message=message) 
-    history.add_ai_message(message=reply) 
+    history.add_user_message(message=message)
+    history.add_ai_message(message=reply)
 
     # Send the reply back to the WhatsApp number
+    twilio_client.messages.create(
+        from_="whatsapp:+14155238886",
+        body=reply,
+        to=f"whatsapp:+{recipient}",
+    )
     response = MessagingResponse()
     response.message(reply)
     return str(response)
-
 
 
 # @app.route("/random_message", methods=["POST"])
@@ -106,18 +107,18 @@ def chat():
 #     recipient = recipients_db.aggregate([{ "$sample": { "size": 1 } }]).next()["id"]
 #     history = get_recipient_chat_history(recipient)
 #     random_ai_message = create_random_message(clean_history=clean_history(history=history))
-    
+
 # ################################## Send the message to the recipient  #################################
 #     try:
 #         Client(os.environ.get("TWILIO_ACCOUNT_SID"), os.environ.get("TWILIO_AUTH_TOKEN")).messages.create(to=recipient, from_="whatsapp:+14155238886", body=random_ai_message)
-#         history.history.add_ai_message(message=random_ai_message) 
+#         history.history.add_ai_message(message=random_ai_message)
 #     except Exception as e:
 #         print(str(e))
 #         return str(e)
-    
+
 #     return str(random_ai_message)
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=os.getenv("PORT", default=5000)) 
+    app.run(debug=True, port=os.getenv("PORT", default=5000))
     recipients_database.client.close()
