654b8309e4d7261f74f175563740125c6057a6b3
Tarmica <68661064+lordskyzw@users.noreply.github.com>
2023-09-06 10:08:14 +0200

    blueticks in pygwan

--- pygwan/__init__.py
+++ pygwan/__init__.py
@@ -584,7 +584,7 @@ class WhatsApp(object):
             headers=headers,
             json=json_data,
         ).json()
-        if response.status_code == 200:
+        if response:
             logging.info(f"Message {message_id} marked as read")
             return response
         logging.info(f"Error marking message {message_id} as read")
